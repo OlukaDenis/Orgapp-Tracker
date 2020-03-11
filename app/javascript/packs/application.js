168711ae87a7ad28de0a08e69c54344b30efc306
OlukaDenis <olukadeno@gmail.com>
2020-03-11 19:36:01 +0300

    App UI enhancement 1. Added a side navbar with links 2. Designed the projects and groups screens

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
