4b44a73f162053adbd663c487f320619173e23df
OlukaDenis <olukadeno@gmail.com>
2020-03-13 12:42:56 +0300

    Removed region from carrierwave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,11 +4,11 @@ if Rails.env.production?
         # Configuration for Amazon S3
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :region                => ENV['S3_REGION']
+        :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        # :region                => ENV['S3_REGION']
         }
         config.fog_directory     =  ENV['S3_BUCKET']
-        config.fog_provider = 'fog/aws'
+        # config.fog_provider = 'fog/aws'
 
     end
 end
